8490ed5643ec4c9f6dfbbdc34395ae18e7a6d7a5
cherepenin dima <dima.cher@gmail.com>
2025-11-13 09:30:14 +0300

    some changes

--- Program.cs
+++ Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("Current date: " + DateTime.Now.ToString("yyyy-MM-dd"));
+        Console.WriteLine("Group: [Ваш номер группы]");
+        Console.WriteLine("Name: [Ваше ФИО]");
     }
 }
