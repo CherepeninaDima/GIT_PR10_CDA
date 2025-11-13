9bc95892c6843f280d180b364fc314090e554ee1
cherepenin dima <dima.cher@gmail.com>
2025-11-13 09:50:15 +0300

    iron out the kinks again 1

---
+++ eskere/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Current date: " + DateTime.Now.ToString("yyyy-MM-dd"));
+        Console.WriteLine("Group: [Ваш номер группы]");
+        Console.WriteLine("Name: [Ваше ФИО]");
+    }
+}
